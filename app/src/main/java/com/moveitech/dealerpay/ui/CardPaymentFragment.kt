262f647b225fad62226064a84a759e1063f6b2a4
anserabbas <anserabbas82@gmail.com>
2022-06-18 17:51:38 +0500

    pull req

--- app/src/main/java/com/moveitech/dealerpay/ui/CardPaymentFragment.kt
+++ app/src/main/java/com/moveitech/dealerpay/ui/CardPaymentFragment.kt
@@ -1,23 +1,22 @@
-package com.moveitech.dealerpay
+package com.moveitech.dealerpay.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.moveitech.dealerpay.databinding.FragmentHomeBinding
+import com.moveitech.dealerpay.databinding.FragmentCardPaymentBinding
 import com.moveitech.dealerpay.databinding.FragmentLoginBinding
 import com.moveitech.dealerpay.ui.BaseFragment
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+class CardPaymentFragment : BaseFragment<FragmentCardPaymentBinding>() {
     override fun initViews() {
     }
 
     override fun getFragmentBinding(
         layoutInflater: LayoutInflater,
         container: ViewGroup?
-    ) = FragmentHomeBinding.inflate(layoutInflater, container, false)
-
+    ) = FragmentCardPaymentBinding.inflate(layoutInflater, container, false)
 
     override fun setDefaultUi() {
     }
--- app/src/main/java/com/moveitech/dealerpay/ui/HomeFragment.kt
+++ app/src/main/java/com/moveitech/dealerpay/ui/HomeFragment.kt
@@ -1,22 +1,23 @@
-package com.moveitech.dealerpay
+package com.moveitech.dealerpay.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.moveitech.dealerpay.databinding.FragmentCardPaymentBinding
+import com.moveitech.dealerpay.databinding.FragmentHomeBinding
 import com.moveitech.dealerpay.databinding.FragmentLoginBinding
 import com.moveitech.dealerpay.ui.BaseFragment
 
-class CardPaymentFragment : BaseFragment<FragmentCardPaymentBinding>() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun initViews() {
     }
 
     override fun getFragmentBinding(
         layoutInflater: LayoutInflater,
         container: ViewGroup?
-    ) = FragmentCardPaymentBinding.inflate(layoutInflater, container, false)
+    ) = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
 
     override fun setDefaultUi() {
     }
